8f9430848eac0bb2aba8cfc29e791aaea3b0ca27
Aashita <aashita@luckyseat.com>
2020-08-23 15:59:04 -0400

    formatting

--- hooks/useTopHeadlines.ts
+++ hooks/useTopHeadlines.ts
@@ -1,53 +1,51 @@
 import { useState, useEffect } from "react";
 
 export type Author = {
-	"@type": string;
-	name: string;
-	url: string;
-	jobTitle: string;
-}
+  "@type": string;
+  name: string;
+  url: string;
+  jobTitle: string;
+};
 
 export type Article = {
-	source: {
-		id: string;
-		name: string;
-	},
-	author?: string;
-	title: string;
-	description: string;
-	url: string;
-	urlToImage: string;
-	publishedAt: Date;
-	content: string;
-}
+  source: {
+    id: string;
+    name: string;
+  };
+  author?: string;
+  title: string;
+  description: string;
+  url: string;
+  urlToImage: string;
+  publishedAt: Date;
+  content: string;
+};
 
 export type News = {
-	topHeadlines: {
-		status: "ok" | "status";
-		totalResults: number;
-		articles: Array<Article>;
-	}
-}
+  topHeadlines: {
+    status: "ok" | "status";
+    totalResults: number;
+    articles: Array<Article>;
+  };
+};
 
 const useTopHeadlines = (query?: string) => {
-    const [status, setStatus] = useState('idle');
-    const [data, setData] = useState<News>();
+  const [status, setStatus] = useState("idle");
+  const [data, setData] = useState<News>();
 
-    useEffect(() => {
-        const fetchData = async () => {
-            setStatus('fetching');
-            const response = await fetch(
-							query ? `/api/news/${query}` : "/api/news"
-            );
-						const data = await response.json();
-            setData(data);
-            setStatus('fetched');
-        };
+  useEffect(() => {
+    const fetchData = async () => {
+      setStatus("fetching");
+      const response = await fetch(query ? `/api/news/${query}` : "/api/news");
+      const data = await response.json();
+      setData(data);
+      setStatus("fetched");
+    };
 
-        fetchData();
-    }, [query]);
+    fetchData();
+  }, [query]);
 
-    return { status, data };
+  return { status, data };
 };
 
 export default useTopHeadlines;
